94ea5c3d77a02bb0ee729a212ed4017bcb5a6835
Shahzad Arif <sarif@splunk.com>
2023-08-05 21:35:37 +0100

    Fix the file

--- main.tf
+++ main.tf
@@ -1,13 +1,21 @@
-data "aws_ami" "app_ami" {
+data "aws_ami" "ubuntu" {
   most_recent = true
+  owners      = ["679593333241"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-focal-20.04-*"]
   }
-  
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = ami-0b594cc165f9cddaa
+  ami              = data.aws_ami.app_ami.id
   instance_type = "t2.micro"
 
-  tags = {
-    Name = "HelloWorld"
-    Department = "Engineering"
-  }
+  tags = { Name = "HelloWorld" }
 }
