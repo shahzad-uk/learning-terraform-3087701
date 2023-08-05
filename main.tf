1b7a767a78f6d8f023a98c699829e36ad6dc0cc2
Shahzad Arif <sarif@splunk.com>
2023-08-05 21:37:35 +0100

    Fix the file

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami              = data.aws_ami.app_ami.id
+  ami              = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = { Name = "HelloWorld" }
