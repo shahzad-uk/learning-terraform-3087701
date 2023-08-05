25bf5e0844aea66fd1e99236347574336a9303a8
Shahzad Arif <sarif@splunk.com>
2023-08-05 09:58:19 +0100

    Renamed main and simplified it

--- main.tf
+++ main.tf
@@ -1,24 +1,9 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = ami-0b594cc165f9cddaa
   instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
+    Department = "Engineering"
   }
 }
